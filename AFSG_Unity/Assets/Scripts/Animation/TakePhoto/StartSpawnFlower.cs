6c76ae65c534ab0108698b953b66972f4cd70d96
TaMi <gary321588@gmail.com>
2025-02-11 17:50:06 +0800

    feat: flower spawner

---
+++ AFSG_Unity/Assets/Scripts/Animation/TakePhoto/StartSpawnFlower.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class StartSpawnFlower : StateMachineBehaviour
+{
+    
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<FlowerInteraction>().ToogleSpawningFlower(true);
+    }
+    
+}
---
+++ AFSG_Unity/Assets/Scripts/Animation/TakePhoto/StopSpawnFlower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Animation
+{
+    public class StopSpawnFlower : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo,
+            int layerIndex)
+        {
+            animator.GetComponent<FlowerInteraction>().ToogleSpawningFlower(false);
+        }
+    }
+}
